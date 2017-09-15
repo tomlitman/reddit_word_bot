dff85f30634cf1ab188373351075802e7047d0d1
jack dymond <JackDymond@jacks-MacBook-Pro.local>
2017-09-15 21:42:18 +0100

    replies

--- WordBot.py
+++ WordBot.py
@@ -1,18 +1,25 @@
 import praw
 
+from urllib.parse import quote_plus
+
 reddit = praw.Reddit('word_bot', user_agent='Mac OS:WordBotv0.1 (by /u/J_Dymond)')
 
-subreddit = reddit.subreddit('SaintsFC')
+subreddit = reddit.subreddit('AbsolutelyNormal')
 
-#for submission in subreddit.hot(limit=10):
-#    print(submission.title)  # Output: the submission's title
-#    print(submission.score)  # Output: the submission's score
-#    print(submission.id)     # Output: the submission's ID
-#    print(submission.url) 
+# for submission in subreddit.hot(limit=10):
+#     print(submission.title)  # Output: the submission's title
+#     print(submission.score)  # Output: the submission's score
+#     print(submission.id)     # Output: the submission's ID
+#     print(submission.url) 
     
-submission = reddit.submission(id='708wzq')
+submission = reddit.submission(id='70cc44')
 submission.comment_sort = 'new'
+
 for top_level_comment in submission.comments:
-    print(top_level_comment.body)
+    body = top_level_comment.body
+    print(body)
+    if 'Reply to this' in body:
+        top_level_comment.reply('ok')
+        break
 
 
