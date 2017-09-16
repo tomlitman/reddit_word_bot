33bc68477ffe2ba162595e7be987038c074d15fd
jack dymond <JackDymond@jacks-MacBook-Pro.local>
2017-09-16 06:55:55 +0100

    Oxford API stuff added

--- WordBot.py
+++ WordBot.py
@@ -1,4 +1,11 @@
 import praw
+import requests
+import json
+from pprint import pprint
+
+#for OED API
+app_id = '5c9096e2'
+app_key = '50c202c6ebe0c06fc6beee5bf49fe152'
 
 from urllib.parse import quote_plus
 
@@ -6,11 +13,11 @@ reddit = praw.Reddit('word_bot', user_agent='Mac OS:WordBotv0.1 (by /u/J_Dymond)
 
 subreddit = reddit.subreddit('AbsolutelyNormal')
 
+
 # for submission in subreddit.hot(limit=10):
 #     print(submission.title)  # Output: the submission's title
 #     print(submission.score)  # Output: the submission's score
-#     print(submission.id)     # Output: the submission's ID
-#     print(submission.url) 
+#     print(submission.id)     # Output: the submission's ID 
     
 submission = reddit.submission(id='70cc44')
 submission.comment_sort = 'new'
@@ -18,8 +25,17 @@ submission.comment_sort = 'new'
 for top_level_comment in submission.comments:
     body = top_level_comment.body
     print(body)
-    if 'Reply to this' in body:
-        top_level_comment.reply('ok')
-        break
+    if 'Awry' in body:
+    	
+    	language = 'en'
+    	word_id = 'Awry'
+    	url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower()
+    	r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
+    	break
 
 
+print("text \n" + r.text)
+
+WordInfo = json.loads(r.text)['results']
+
+print(WordInfo)
