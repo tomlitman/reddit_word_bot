69ca0e13e25117757488d8ee889d11ea980f3847
jack dymond <JackDymond@jacks-MacBook-Pro.local>
2017-09-17 16:17:52 +0100

    Finding Words and replying is now a function, that takes the submission as a parameter, goes through the subreddit, and performs the function on each submission

--- WordBot.py
+++ WordBot.py
@@ -6,44 +6,50 @@ import requests
 import json
 import string
 
+reddit = praw.Reddit('word_bot', user_agent='Mac OS:WordBotv0.1 (by /u/J_Dymond)')
+subreddit = reddit.subreddit('AbsolutelyNormal')
 
 # gets top definition of word,
 def get_def(word):
-    language = 'en'
-    word_id = word
-    url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower()
-    r = requests.get(url, headers={'app_id': app_id, 'app_key': app_key})
 
-    # code = 200, means the word is in the dictionary
-    if r.status_code != 200:
-        return ''
+	# for OED API
+	app_id = '5c9096e2'
+	app_key = '50c202c6ebe0c06fc6beee5bf49fe152'
+	language = 'en'
+	word_id = word
+	url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower()
+	r = requests.get(url, headers={'app_id': app_id, 'app_key': app_key})
 
-    # below is json for word info, un comment to see the data structure
-    # print("text \n" + r.text)
+	# code = 200, means the word is in the dictionary
+	if r.status_code != 200:
+		return ''
 
-    word_info = json.loads(r.text)['results']
+	# below is json for word info, un comment to see the data structure
+	# print("text \n" + r.text)
 
-    word_definitions = word_info[0]['lexicalEntries'][0]['entries'][0]['senses']
+	word_info = json.loads(r.text)['results']
 
-    # this bit gets the top definition, it's kind of fucked due to the formatting of the request result
-    # probably need to rewrite, but atm isn't returning an error
-    while True:
-        try:
-            def1 = word_definitions[0]['definitions'][0]
-            break
-        except KeyError:
-            try:
-                def1 = word_definitions[0]['subsenses'][0]['definitions'][0]
-            except KeyError:
-                def1 = word_definitions[0]['crossReferenceMarkers'][0]
-            break
+	word_definitions = word_info[0]['lexicalEntries'][0]['entries'][0]['senses']
 
-    reply = ('>' + word + ': \n\n ' + def1 + '\n\n')
+	# this bit gets the top definition, it's kind of fucked due to the formatting of the request result
+	# probably need to rewrite, but atm isn't returning an error
+	while True:
+		try:
+			def1 = word_definitions[0]['definitions'][0]
+			break
+		except KeyError:
+			try:
+				def1 = word_definitions[0]['subsenses'][0]['definitions'][0]
+			except KeyError:
+				def1 = word_definitions[0]['crossReferenceMarkers'][0]
+			break
 
-    # ^^ all of this must have an easier solution, maybe traversing the json ^^
-    #    rather than using specific guesses
+	reply = ('>' + word + ': \n\n ' + def1 + '\n\n')
 
-    return reply
+	# ^^ all of this must have an easier solution, maybe traversing the json ^^
+	#    rather than using specific guesses
+
+	return reply
 
 # gets words from a comment, puts them into an array, removes punctuation
 def get_words(comment):
@@ -61,34 +67,27 @@ def word_search(word):
     return word in word_list
     
 
-# for OED API
-app_id = '5c9096e2'
-app_key = '50c202c6ebe0c06fc6beee5bf49fe152'
+def FindAndReply(submission):
 
-# I have no idea what this does, or if it's useful
-# from urllib.parse import quote_plus
+	submission.comment_sort = 'new'
 
-reddit = praw.Reddit('word_bot', user_agent='Mac OS:WordBotv0.1 (by /u/J_Dymond)')
+	for top_level_comment in submission.comments:
 
-subreddit = reddit.subreddit('AbsolutelyNormal')
-
-# for submission in subreddit.hot(limit=10):
-#     print(submission.title)  # Output: the submission's title
-#     print(submission.score)  # Output: the submission's score
-#     print(submission.id)     # Output: the submission's ID 
-
-submission = reddit.submission(id='70cc44')
-submission.comment_sort = 'new'
-
-for top_level_comment in submission.comments:
-
-	Comment_Words = get_words(top_level_comment.body)
+		Comment_Words = get_words(top_level_comment.body)
 	
-	for word in Comment_Words:
+		for word in Comment_Words:
 
-		if word_search(word) == True:
-			reply = (get_def(word))
-			print(reply)
+			if word_search(word) == True:
+				reply = (get_def(word))
+				print(reply)
 			
-			top_level_comment.reply(reply)
-			break
+				#top_level_comment.reply(reply)
+				break
+    
+
+for submission in subreddit.hot(limit=10):
+
+	 print(submission.title)  # Output: the submission's title
+	 print(submission.id)     # Output: the submission's ID 
+	 FindAndReply(submission)
+
