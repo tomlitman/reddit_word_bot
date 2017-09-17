111569922f340b47b746f09bfdaef45a084bcecc
jack dymond <JackDymond@jacks-MacBook-Pro.local>
2017-09-17 15:48:27 +0100

    Cleaned a little lol

--- WordBot.py
+++ WordBot.py
@@ -6,25 +6,6 @@ import requests
 import json
 import string
 
-# for OED API
-app_id = '5c9096e2'
-app_key = '50c202c6ebe0c06fc6beee5bf49fe152'
-
-# I have no idea what this does, or if it's useful
-# from urllib.parse import quote_plus
-
-reddit = praw.Reddit('word_bot', user_agent='Mac OS:WordBotv0.1 (by /u/J_Dymond)')
-
-subreddit = reddit.subreddit('AbsolutelyNormal')
-
-# for submission in subreddit.hot(limit=10):
-#     print(submission.title)  # Output: the submission's title
-#     print(submission.score)  # Output: the submission's score
-#     print(submission.id)     # Output: the submission's ID 
-
-submission = reddit.submission(id='70cc44')
-submission.comment_sort = 'new'
-
 
 # gets top definition of word,
 def get_def(word):
@@ -64,7 +45,6 @@ def get_def(word):
 
     return reply
 
-
 # gets words from a comment, puts them into an array, removes punctuation
 def get_words(comment):
     comment_words = []
@@ -74,13 +54,31 @@ def get_words(comment):
         comment_words.append(word)
     return comment_words
 
-
 # if word is in relevant list, will return true.
 def word_search(word):
     # lazily gets the relevant word list from Words.py, by using the first letter in the word.
     word_list = eval('Words.' + str.lower(word[:1]))
     return word in word_list
+    
 
+# for OED API
+app_id = '5c9096e2'
+app_key = '50c202c6ebe0c06fc6beee5bf49fe152'
+
+# I have no idea what this does, or if it's useful
+# from urllib.parse import quote_plus
+
+reddit = praw.Reddit('word_bot', user_agent='Mac OS:WordBotv0.1 (by /u/J_Dymond)')
+
+subreddit = reddit.subreddit('AbsolutelyNormal')
+
+# for submission in subreddit.hot(limit=10):
+#     print(submission.title)  # Output: the submission's title
+#     print(submission.score)  # Output: the submission's score
+#     print(submission.id)     # Output: the submission's ID 
+
+submission = reddit.submission(id='70cc44')
+submission.comment_sort = 'new'
 
 for top_level_comment in submission.comments:
 
